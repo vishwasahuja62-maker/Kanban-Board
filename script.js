e57d3408c7d7eff3613ccb4c4bb6dfa5193c35a6
Vishwas Ahuja <vishwasahuja62@gmail.com>
2026-02-13 23:55:00 +0530

    Implementation of Add New Task Logic

--- script.js
+++ script.js
@@ -34,9 +34,49 @@ function addDragEventsOnColumn(column) {
         column.appendChild(dragElement);
         column.classList.remove("hover-over");
     })
+
+
 }
 
 addDragEventsOnColumn(todo);
 addDragEventsOnColumn(progress);
 addDragEventsOnColumn(done);
 
+// Modal Related Logic
+const toggleModalButton = document.querySelector('#toggle-modal');
+const modalBg = document.querySelector('.modal .bg');
+const modal = document.querySelector('.modal');
+const addTaskButton = document.querySelector('#add-new-task');
+
+toggleModalButton.addEventListener("click", () => {
+    modal.classList.toggle("active");
+})
+
+modalBg.addEventListener("click", () => {
+    modal.classList.remove("active");
+})
+
+addTaskButton.addEventListener("click", () => {
+
+    const taskTitle = document.querySelector('#task-title-input').value
+    const taskDesc = document.querySelector('#task-desc-input').value
+
+    const div = document.createElement("div");
+
+    div.classList.add("task");
+    div.setAttribute("draggable", "true");
+    div.innerHTML = `
+        <h2>${taskTitle}</h2>
+        <p>${taskDesc}</p>
+        <button>Delete</button>
+    `;
+    todo.appendChild(div);
+
+    div.addEventListener("drag", (e) => {
+        dragElement = div;
+    })
+
+    modal.classList.remove("active");
+
+
+})
